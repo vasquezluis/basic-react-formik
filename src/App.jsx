54f33e94ea3fb9b3a782d38ebefc52ca44a73b43
LuiiVasquez <luivasque95@gmail.com>
2023-03-06 23:00:15 -0600

    added a new example of useFormik hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+
 import Form from "./components/Form";
+import BasicForm from "./components/BasicFormYUP";
+import AdvanceForm from "./components/AdvanceFormYUP";
 import "./App.css";
 
 function App() {
+  const [view, setView] = useState("basic");
+
   return (
-    <div className="grid h-screen place-items-center">
-      <Form />
+    <div className="flex justify-center content-center">
+      {/* <Form /> */}
+
+      <div className="h-[400px] w-[400px] ">
+        <nav className="w-full h-full">
+          <h3
+            className={`px-1 py-1 rounded bg-blue-500 cursor-pointer ${
+              view == "basic" ? "color-white" : ""
+            }`}
+            onClick={() => setView("basic")}
+          >
+            Basic
+          </h3>
+          <h3
+            className={`px-1 py-1 rounded bg-green-500 cursor-pointer ${
+              view == "advance" ? "color-white" : ""
+            }`}
+            onClick={() => setView("advance")}
+          >
+            Advance
+          </h3>
+        </nav>
+
+        <div className="w-full h-full">
+          {view == "basic" ? <BasicForm /> : <AdvanceForm />}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/BasicFormYUP.jsx
@@ -0,0 +1,127 @@
+import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
+
+import "./BasicFormYUP.css";
+
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  actions.resetForm();
+};
+
+function Form() {
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: basicSchema,
+    onSubmit,
+  });
+
+  return (
+    <div className="">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label htmlFor="email" className="pr-2">
+            Email
+          </label>
+          <input
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            className={errors.email && touched.email ? "input-error" : ""}
+          />
+          {errors.email && touched.email && (
+            <p className="error">{errors.email}</p>
+          )}
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="age" className="pr-2">
+            Age
+          </label>
+          <input
+            value={values.age}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="text"
+            name="age"
+            id="age"
+            placeholder="Enter your age"
+            className={errors.age && touched.age ? "input-error" : ""}
+          />
+          {errors.age && touched.age && <p className="error">{errors.age}</p>}
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="password" className="pr-2">
+            Password
+          </label>
+          <input
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            className={errors.password && touched.password ? "input-error" : ""}
+          />
+          {errors.password && touched.password && (
+            <p className="error">{errors.password}</p>
+          )}
+        </div>
+
+        <div className="mb-2">
+          <label htmlFor="confirmPassword" className="pr-2">
+            Confirm Password
+          </label>
+          <input
+            value={values.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="text"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirm your password"
+            className={
+              errors.confirmPassword && touched.confirmPassword
+                ? "input-error"
+                : ""
+            }
+          />
+          {errors.confirmPassword && touched.confirmPassword && (
+            <p className="error">{errors.confirmPassword}</p>
+          )}
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="rounded font-bold cursor-pointer px-2 py-1 bg-green-400"
+            disabled={isSubmitting}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
